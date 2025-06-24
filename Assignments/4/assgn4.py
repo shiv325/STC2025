cfa16fd58c45adef90a1de0ad74c25f41a3e2d14
Shivam <139581888+shiv325@users.noreply.github.com>
2025-06-24 22:53:00 +0530

    Update assgn4.py

--- Assignments/4/assgn4.py
+++ Assignments/4/assgn4.py
@@ -1,1 +1,45 @@
+from google import genai
 
+client = genai.Client(api_key="AIzaSyCaVdOkYqC2QQe855cljGbsAonqnfKch3Q")
+
+poemTopic = input("Enter the Poem Title => ")
+
+response1 = client.models.generate_content(
+    model = "gemini-2.5-flash",
+    contents = f"Write a Poem on the topic {poemTopic}"
+)
+
+generatedPoem = response1.text
+
+print(f"Gemini =>\n{generatedPoem}\n")
+
+decision = input("Wanna modify the poem? (y/n) => ")
+if (decision == "y") :
+    print("1. Shoten the Poem")
+    print("2. Expand the poem with more detail and imagery")
+    print("3. Rewrite the poem in a different style")
+    print("4. Change the tone of the poem (e.g., joyful, somber, formal, humorous)")
+
+    choice = int(input("Choose the desired option => "))
+    additionalInfo = input("Enter any additional details for modification =>\n")
+    request = f"{generatedPoem}\n\n"
+
+    if choice == 1 :
+        request += "Shorten the Poem"
+    elif choice == 2 :
+        request += "Expand the poem with more detail and imagery"
+    elif choice == 3 :
+        request += "Rewrite the poem in a different style"
+    elif choice == 4 :
+        request += "Change the tone of the poem (e.g., joyful, somber, formal, humorous)"
+    
+    request += f"\n\n{additionalInfo}"
+
+    response2 = client.models.generate_content(
+        model = "gemini-2.5-flash",
+        contents = request
+    )
+
+    modifiedPoem = response2.text
+
+    print(f"Gemini =>\n{modifiedPoem}")
